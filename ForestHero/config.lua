1c0f1b0b0917315145f976bb2db00f1a28ab8f77
alesantos99 <38537348+alesantos99@users.noreply.github.com>
2019-08-26 15:50:54 -0300

    Add files via upload

--- ForestHero/config.lua
+++ ForestHero/config.lua
@@ -7,9 +7,10 @@ application =
 {
 	content =
 	{
-		width = 320,
+     	width = 320,
 		height = 480, 
 		scale = "letterbox",
+		
 		fps = 60,
 		
 		--[[
--- ForestHero/level1.lua
+++ ForestHero/level1.lua
@@ -15,37 +15,118 @@ local physics = require "physics"
 -- forward declarations and other locals
 local screenW, screenH, halfW = display.actualContentWidth, display.actualContentHeight, display.contentCenterX
 
-local hero
+local hero = {}
 
 local buttonLeft
 
 local buttonRight
 
+local buttonJump
+
+local camera 
+-- camera 
+local function moveCamera(event)
+	local leftOffset = 60
+
+	local heroX = hero.x
+	
+	local screenLeft = -camera.x
+
+	local bufferMoveArea = 380
+
+	if heroX > leftOffset then
+		if heroX > screenLeft+ bufferMoveArea then
+			camera.x = -hero.x +leftOffset
+		end
+	else
+		camera.x= 0
+	end
+	
+end
+
+-- hero features
+
+local function heroFeatures()
+	hero.speed = 50
+
+end 
+
+local function heroVelocity()
+	hero:setLinearVelocity(hero.velocity, 0)
+
+end
+local sheetOptions =
+{
+    width = 96,
+    height = 96,
+    numFrames = 4
+}
+
+local moveHero=  graphics.newImageSheet( "spriteshero.png", sheetOptions )
+
+local sequences_hero = {
+    -- first sequence (consecutive frames)
+	{
+        name = "runLeft",
+        start = 6,
+        count = 2,
+        time = 800,
+        loopCount = 0
+    },
+    
+	{
+        name = "runRight",
+        start = 2,
+        count = 3,
+        time = 800,
+        loopCount = 0
+    },
+    -- next sequence (non-consecutive frames)
+    {
+        name = "walking",
+        frames = { 1 },
+        time = 400,
+        loopCount = 0
+    },
+}
+
+hero = display.newSprite( moveHero, sequences_hero )
+local function spriteListener( event )
+ 
+    local thisSprite = event.target  -- "event.target" references the sprite
+ 
+    if ( event.phase == "ended" ) then 
+		thisSprite:setSequence( "run" )  -- switch to "fastRun" sequence
+        thisSprite:play()  -- play the new sequence
+    end
+end
+ 
+-- add the event listener to the sprite
+hero:addEventListener( "sprite", spriteListener )
 
 function scene:create( event )
 
-	-- Called when the scene's view does not exist.
-	-- 
-	-- INSERT code here to initialize the scene
-	-- e.g. add display objects to 'sceneGroup', add touch listeners, etc.
-
+	
+	
 	local sceneGroup = self.view
 
-	-- We need physics started to add bodies, but we don't want the simulaton
-	-- running until the scene is on the screen.
+	camera = display.newGroup()
+	
 	physics.start()
 	physics.pause()
 
-
-	-- create a grey rectangle as the backdrop
-	-- the physical screen will likely be a different shape than our defined content area
-	-- since we are going to position the background from it's top, left corner, draw the
-	-- background at the real top, left corner.
-	local background = display.newImageRect("fundo.png", 2024,2024 )
+		
+	print(display.screenOriginX)
+	local background = display.newImageRect("fundo.png", 2000,display.contentHeight )
 	background.x = display.screenOriginX
-	--local background = display.newRect("fundo.png" ,display.screenOriginX, display.screenOriginY, screenW, screenH )
+	background.y = display.screenOriginY
+
+	print("background:",display.screenOriginX)
+	
 	background.anchorX = 0 
+	
 	background.anchorY = 0
+	
 	background:setFillColor( .5 )
 
 	-- create sum
@@ -55,11 +136,13 @@ function scene:create( event )
 
 	sol.y = 90
 	
-	-- crate cloud
+	-- crate clounds
 	
 	local cloud = display.newImageRect("nuvem.png",100, 70)
 	
 	cloud.x = 180
+
+
 
 	cloud.y = 90
 
@@ -70,21 +153,9 @@ function scene:create( event )
 
 	cloud.y = 100
 
-	local tree = display.newImageRect("arvore.png",100, 112)
-	
-	tree.x = 400
-
-	tree.y = display.contentCenterX-60
-
-	local lenhador = display.newImageRect("lenhador.png",60, 60)
-	
-	lenhador.x = 500
-
-	lenhador.y = display.contentCenterX-50
-
 	
 	-- create ground
-	local ground = display.newImageRect("terra.png",screenW, 70)
+	local ground = display.newImageRect("terra.png",2000, 70)
 
 	ground.anchorX = 0
 
@@ -92,8 +163,10 @@ function scene:create( event )
 
 	ground.x, ground.y = display.screenOriginX, display.actualContentHeight + display.screenOriginY
 
-	local groundShape = { -halfW,-34, halfW,-34, halfW,34, -halfW,34 }
-	physics.addBody( ground, "static", { friction=0.3, shape=groundShape } )
+	
+	--local groundShape = { -halfW,-34, halfW,-34, halfW,34, -halfW,34 }
+	
+	physics.addBody( ground, "static" )
 	
 	local ground1 = display.newImageRect("terra.png",400, 50)
 
@@ -106,51 +179,178 @@ function scene:create( event )
 	local groundShape = { -halfW,-34, halfW,-34, halfW,34, -halfW,34 }
 	physics.addBody( ground1, "static", { friction=0.3, shape=groundShape } )
 
+	local ground2 = display.newImageRect("terra.png",1000, 70)
+
+	ground2.anchorX = 0
+
+	ground2.anchorY = 1
+
+	ground2.x, ground2.y = ground.x+800,  ground.y+20
+
 	
+	
+	--local tree = display.newImageRect("arvore.png",100, 112)
+	
+	--tree.x = 400
 
-	hero = display.newImageRect("hero.png",60,60)
+	--tree.y = display.contentCenterX-60
 
+	--local lenhador = display.newImageRect("lenhador.png",60, 60)
+	
+	--lenhador.x = 500
+
+	--lenhador.y = display.contentCenterX-50
+
+	
+	
+	-- Create hero
+
+	--hero = display.newImageRect("hero.png",60,60)
 	hero.x = 70
 
 	hero.y = display.contentHeight-86
 	
-	character = hero
+	
+	physics.addBody( hero, { density=1, friction=0.3, bounce= 0 } )
 
-	physics.addBody( hero, { density=1.0, friction=0.3, bounce= 0 } )
+	
+	heroFeatures()
+	-- Create controls
+	
+	-- Move to left
+	
+	buttonLeft = display.newImageRect("direcional2.png", 100,100)
 
-	buttonLeft = display.newImageRect("esquerda.png", 100,100)
+	buttonLeft.id = "left"
 
-	buttonLeft.x = 80
+	buttonLeft.x= 10 
 
 	buttonLeft.y = display.contentHeight-20
 	
-	buttonLeft:addEventListener("touch", moveToLeft)
+	buttonLeft:addEventListener("touch",controls)
 
-	buttonRight = display.newImageRect("direita.png", 100,100)
 
-	buttonRight.x = 100
+	-- Move to right
+	buttonRight = display.newImageRect("direcional1.png", 100,100)
+
+	buttonRight.id = "right"
+
+	buttonRight.x = 70
 
 	buttonRight.y = display.contentHeight-20
-	buttonRight:addEventListener("touch", moveToRight)
+	buttonRight:addEventListener("touch", controls)
 
 	
-	sceneGroup:insert( background )
-	sceneGroup:insert( ground )
-	sceneGroup:insert( hero )
+	
+	
+	-- Jump
+	buttonJump = display.newImageRect("pular.png", 100,100)
+
+	buttonJump.id = "jump"
+
+	buttonJump.x = 450
+
+	buttonJump.y = display.contentHeight-20
+
+	buttonJump:addEventListener("touch", controls)
+
+	
+	-- Insert elements that follow the camera 
+	
+	camera:insert(background)
+
+	camera:insert(ground)
+
+	camera:insert(ground1)
+	
+	camera:insert(hero)
+
+	
+
+	-- Isert elements into the scene
+	
+	sceneGroup:insert( camera)
+	
 	sceneGroup:insert( buttonLeft )
+	
 	sceneGroup:insert( buttonRight )
+	
+	sceneGroup:insert( buttonJump )
+	
 	sceneGroup:insert( sol)
+	
 	sceneGroup:insert( cloud)
 	
-	--sceneGroup:insert( grass)
+end
+
+function controls(event)
 	
+	local pressed = event.target
+
+	if event.phase == "began" then
+
+		if pressed.id == "jump" then
+			display.currentStage:setFocus(buttonJump)
+				
+			hero:applyLinearImpulse(0,-50, hero.x, hero.y)
+			hero:setSequence( "walking" )  -- switch to "fastRun" sequence
+        	hero:play()
+			
+		else
+			local leftEdge = display.screenOriginX+150
+			print(leftEdge)
+			
+			if pressed.id == "left" then
+				if hero.x > leftEdge then	
+				
+				display.currentStage:setFocus(buttonLeft)
+				
+				print("Hero x",hero.x)
+				print("Hero y",hero.y)
+
+				hero:applyLinearImpulse(-10,0, hero.x, hero.y)
+				hero:setSequence( "runLeft" )  -- switch to "fastRun" sequence
+        		hero:play()
+				hero.velocity = -hero.speed
+				
+			end
+			elseif pressed.id=="right" and hero.x <1400  then
+				display.currentStage:setFocus(buttonRight)
+
+				hero:applyLinearImpulse(10,0, hero.x, hero.y)
+				hero:setSequence( "runRight" )  -- switch to "fastRun" sequence
+        		hero:play()
+				hero.velocity = hero.speed
+				
+			end	
+				
+	
+		end
+	elseif event.phase == "ended" then
+		
+		if pressed.id == "jump" then
+			
+			display.currentStage:setFocus(nil)
+			
+		elseif pressed.id == "left" then
+			
+			display.currentStage:setFocus(nil)
+		
+		elseif pressed.id == "right" then
+			
+			display.currentStage:setFocus(nil)
+		end
+
+	end
+
+
 end
 
 function moveToLeft(event) 
 	buttonLeft = event.target
 	if event.phase == "began" then
 		display.currentStage:setFocus(buttonLeft)
-		hero:applyLinearImpulse(-10,0, hero.x, hero.y)
+		hero:applyLinearImpulse(-5,0, hero.x, hero.y)
 	
 	end
 end
@@ -160,15 +360,31 @@ function moveToRight(event)
 	
 	if event.phase == "began" then
 		display.currentStage:setFocus(buttonRight)
-		hero:applyLinearImpulse(10,0, hero.x, hero.y)
+		hero:applyLinearImpulse(5,0, hero.x, hero.y)
 		
 
 	end
 end
 
+function jump(event) 
+
+	buttonJump  = event.target
+
+	if event.phase == "began" then
+		display.currentStage:setFocus(buttonJump)
+		hero:applyLinearImpulse(0,-30, hero.x, hero.y)
+		
+
+	end
+
+
+end
+
 function scene:show( event )
 	local sceneGroup = self.view
 	local phase = event.phase
+
+
 	
 	if phase == "will" then
 		-- Called when the scene is still off screen and is about to move on screen
@@ -177,6 +393,9 @@ function scene:show( event )
 		-- 
 		-- INSERT code here to make the scene come alive
 		-- e.g. start timers, begin animation, play audio, etc.
+		Runtime:addEventListener("enterFrame", moveCamera)
+		
+		
 		physics.start()
 	end
 end
@@ -219,6 +438,8 @@ scene:addEventListener( "hide", scene )
 scene:addEventListener( "destroy", scene )
 scene:addEventListener( "moveToLeft", scene )
 scene:addEventListener( "moveToRight", scene )
+scene:addEventListener( "jump", scene )
+scene:addEventListener("controls",scene)
 
 -----------------------------------------------------------------------------------------
 
