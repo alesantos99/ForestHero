d8cc75cd6f6a97cf82edd1808750439031974bb1
alesantos99 <38537348+alesantos99@users.noreply.github.com>
2019-09-02 17:51:54 -0300

    Add files via upload

---
+++ ForestHero_v0.1/Level1.tsx
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="Level1" tilewidth="579" tileheight="282" tilecount="4" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <terraintypes>
+  <terrain name="Novo Terreno" tile="0"/>
+  <terrain name="Novo Terreno" tile="0"/>
+ </terraintypes>
+ <tile id="0">
+  <image width="504" height="211" source="../../Assets/arvores.png"/>
+ </tile>
+ <tile id="1" terrain="0,0,0,0">
+  <image width="148" height="83" source="../../Assets/g10710.png"/>
+ </tile>
+ <tile id="2">
+  <image width="83" height="30" source="../../Assets/path10716.png"/>
+ </tile>
+ <tile id="3">
+  <image width="579" height="282" source="../../Assets/arbusto.png"/>
+ </tile>
+</tileset>
